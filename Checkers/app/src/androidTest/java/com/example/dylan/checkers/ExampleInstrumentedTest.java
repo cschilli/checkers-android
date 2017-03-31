891b5c95dce0750e33f45956dd11c3539d2331ef
Christopher Schilling <cschilli@uno.edu>
2017-03-31 00:50:11 -0500

    Testing travis

--- Checkers/app/src/androidTest/java/com/example/dylan/checkers/ExampleInstrumentedTest.java
+++ Checkers/app/src/androidTest/java/com/example/dylan/checkers/ExampleInstrumentedTest.java
@@ -1,8 +1,11 @@
 package com.example.dylan.checkers;
 
+
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.filters.LargeTest;
+import
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,12 +18,24 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.dylan.checkers", appContext.getPackageName());
+    }
+
+    @Test
+    public void sayHello() {
+        onView(withText("Say hello!")).perform(click());
+        onView(withId(R.id.textView)).check(matches(withText("Hello, World!")));
     }
 }
