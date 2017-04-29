291fceea0f581864779128165b23a38ac8eb9e6d
Christopher Schilling <cschilli@uno.edu>
2017-04-28 21:38:39 -0500

    Added Player tests

---
+++ Checkers/app/src/test/java/com/uno/dbbc/checkers/PlayerTests.java
@@ -0,0 +1,32 @@
+package com.uno.dbbc.checkers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+/**
+ * Created by Chris on 4/28/2017.
+ */
+
+public class PlayerTests {
+
+
+    @Test
+    public void testPlayer() throws Exception{
+        Player player = new Player("Dark");
+
+        assertEquals("Dark", player.getColor());
+        assertNotEquals("Light", player.getColor());
+    }
+
+    @Test
+    public void testHasMoves() throws Exception{
+        Player player = new Player("Dark");
+        Board board = new Board();
+        assertTrue(player.hasMoves(board));
+
+
+
+    }
+
+
+}
